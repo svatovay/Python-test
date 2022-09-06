98015bb73fc944a8deb9bafea6937bd5f1746fd1
alexeysvatov <zombeaver@gmail.com>
2022-09-06 10:53:33 +0500

    add filter in get-cities

--- src/main.py
+++ src/main.py
@@ -29,7 +29,10 @@ def create_city(city: str = Query(description="Название города", d
 def cities_list(q: str = Query(description="Название города", default=None)):
     """
     Получение списка городов
+    Фильтрация по q
     """
+    if q:
+        return Session().query(City).filter(City.name == q).all()
     cities = Session().query(City).all()
 
     return [{'id': city.id, 'name': city.name, 'weather': city.weather} for city in cities]
