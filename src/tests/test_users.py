55400ab9f0af6a3dda7809bcb62ee8d95e8af87a
alexeysvatov <zombeaver@gmail.com>
2022-09-08 18:08:01 +0500

    add test users

---
+++ src/tests/test_users.py
@@ -0,0 +1,23 @@
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+
+def test_read_users():
+    response = client.get("/users")
+    assert response.status_code == 200
+
+
+def test_read_user():
+    response = client.get("/users/", params={'user_id': 1})
+    assert response.status_code == 200
+    assert list(response.json()[0].keys()) == ['name', 'surname', 'age', 'id']
+
+
+def test_add_user():
+    response = client.post("/users/", json={'name': 'test', 'surname': 'testerov', 'age': 150})
+    assert response.status_code == 200
+    assert response.json()['name'] == 'test'
+    assert list(response.json().keys()) == ['name', 'surname', 'age', 'id']
