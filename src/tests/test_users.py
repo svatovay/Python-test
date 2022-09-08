880a89b10732efd9f5a498c496abb8691d3a55d1
alexeysvatov <zombeaver@gmail.com>
2022-09-08 18:11:07 +0500

    correct test users

--- src/tests/test_users.py
+++ src/tests/test_users.py
@@ -11,9 +11,9 @@ def test_read_users():
 
 
 def test_read_user():
-    response = client.get("/users/", params={'user_id': 1})
+    response = client.get("/users/1")
     assert response.status_code == 200
-    assert list(response.json()[0].keys()) == ['name', 'surname', 'age', 'id']
+    assert list(response.json().keys()) == ['name', 'surname', 'age', 'id']
 
 
 def test_add_user():
